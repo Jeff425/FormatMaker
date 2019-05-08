0c3b386ca88d1a271a533a8e81f3f59349bd128f
Jeff425 <goodhue.jeff@gmail.com>
2019-05-07 23:34:05 -0700

    Footer now opens new tabs for all links, navigation bar will hide after selection on smaller screens

--- src/Footer.js
+++ src/Footer.js
@@ -9,9 +9,9 @@ class Footer extends Component {
         <Container className="mt-3 mb-3">        
           <ul className="d-inline-block mx-auto">
             <li><Button variant="link" onClick={() => window.scrollTo(0, 0)}>Back to top</Button></li>
-            <li><a href="https://www.paypal.me/khalorlstudios" target="_blank">Support FormatMaker</a></li>
-            <li><a href="mailto:khalorlstudios@gmail.com?subject=FormatMaker Question">Contact KhalorlStudios</a></li>
-            <li><a href="http://khalorlstudios.com/">Visit KhalorlStudios</a></li>          
+            <li><a href="https://www.paypal.me/khalorlstudios" target="_blank" rel="noopener noreferrer">Support FormatMaker</a></li>
+            <li><a href="mailto:khalorlstudios@gmail.com?subject=FormatMaker Question" target="_blank" rel="noopener noreferrer">Contact KhalorlStudios</a></li>
+            <li><a href="http://khalorlstudios.com/" target="_blank" rel="noopener noreferrer">Visit KhalorlStudios</a></li>          
           </ul>
           <ul className="text-secondary mt-2 copyright">
             <li>The literal and graphical information presented on this site about Magic: The Gathering, including card images, the mana symbols, and Oracle text, is copyright Wizards of the Coast, LLC, a subsidiary of Hasbro, Inc. This website is not produced by, endorsed by, supported by, or affiliated with Wizards of the Coast.</li>
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -3,29 +3,39 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import { Link, NavLink } from 'react-router-dom';
-import FirebaseContext from './Firebase/FirebaseContext';
+import { withFirebase } from './Firebase/FirebaseContext';
 import ROUTES from './ROUTES';
 
 class NavigationBar extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {expanded: false};
+    this.onSignOut = this.onSignOut.bind(this);
+  }
+  
+  onSignOut() {
+    this.setState({expanded: false});
+    this.props.firebase.signOut();
+  }
+  
   render() {
     return (
-      <Navbar variant="dark" bg="primary" expand="lg" fixed="top">
-        <Navbar.Brand as={Link} to="/">FormatMaker</Navbar.Brand>
+      <Navbar variant="dark" bg="primary" expand="lg" fixed="top" expanded={this.state.expanded} onToggle={() => this.setState({expanded: !this.state.expanded})}>
+        <Navbar.Brand as={Link} to="/" onClick={() => this.setState({expanded: false})}>FormatMaker</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse id="navbarCollapse">
           <Nav>
-            <Nav.Link as={NavLink} to={ROUTES.format}>Create Format</Nav.Link>
-            <Nav.Link as={NavLink} to={ROUTES.deck}>Build Deck</Nav.Link>
-            <Nav.Link as={NavLink} to={ROUTES.howto}>How To Use</Nav.Link>
+            <Nav.Link as={NavLink} to={ROUTES.format} onClick={() => this.setState({expanded: false})}>Create Format</Nav.Link>
+            <Nav.Link as={NavLink} to={ROUTES.deck} onClick={() => this.setState({expanded: false})}>Build Deck</Nav.Link>
+            <Nav.Link as={NavLink} to={ROUTES.howto} onClick={() => this.setState({expanded: false})}>How To Use</Nav.Link>
           </Nav>
           <Nav className="ml-auto">
             {this.props.authUser &&
-              <Nav.Link as={NavLink} to={ROUTES.ownformat}>Your Formats</Nav.Link>
+              <Nav.Link as={NavLink} to={ROUTES.ownformat} onClick={() => this.setState({expanded: false})}>Your Formats</Nav.Link>
             }
             {this.props.authUser &&
-              <FirebaseContext.Consumer>
-                {firebase => <Button variant="link" className="nav-link border-0" onClick={firebase.signOut}>Sign Out</Button>}             
-              </FirebaseContext.Consumer>
+                <Button variant="link" className="nav-link border-0" onClick={this.onSignOut}>Sign Out</Button>        
             }
             {!this.props.authUser && <Nav.Link as={NavLink} to={ROUTES.signin}>Sign In</Nav.Link>}
           </Nav>
@@ -35,4 +45,4 @@ class NavigationBar extends Component {
   }
 }
 
-export default NavigationBar;
+export default withFirebase(NavigationBar);
