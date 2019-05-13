afd18446a8fe9fcee78be46a131916182dba7e4f
Jeff425 <goodhue.jeff@gmail.com>
2019-05-13 01:10:19 -0700

    Updated icon and alignment

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -28,7 +28,7 @@ class NavigationBar extends Component {
   render() {
     return (
       <Navbar variant="dark" bg="primary" expand="lg" fixed="top" expanded={this.state.expanded} onToggle={() => this.setState({expanded: !this.state.expanded})}>
-        <Navbar.Brand as={Link} to="/" onClick={this.onNavigation}><img alt="" src={process.env.PUBLIC_URL + "/favico.ico"} width="32" height="32" className="d-inline-block align-top mr-1" />FormatBuilder</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" onClick={this.onNavigation}><img alt="" src={process.env.PUBLIC_URL + "/favicon.ico"} width="32" height="32" className="d-inline-block align-top mr-2" /><span className="d-inline-block nav-brand-margin">FormatBuilder</span></Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse id="navbarCollapse">
           <Nav>
