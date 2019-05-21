50ed3e67a0e304149ba6947fe2296de4cb3266f2
Jeff425 <goodhue.jeff@gmail.com>
2019-05-20 20:31:44 -0700

    Clarified on main page what does and doesn't need an account

--- src/MainPage.js
+++ src/MainPage.js
@@ -9,13 +9,19 @@ class MainPage extends Component {
       <div className="main-page">
         <h1>FormatBuilder</h1>
         <h4>Create custom <b>Magic: The Gathering</b> formats and build decks for your formats</h4>
-        <div className="main-page-button-bar">
-          <Link to={ROUTES.format} className="main-page-buttons">
-            <Button variant="primary" size="lg" className="fullWidth">Create Format</Button>
-          </Link>
-          <Link to={ROUTES.deck} className="main-page-buttons">
-            <Button variant="primary" size="lg" className="fullWidth">Build Deck</Button>
-          </Link>
+        <div className="main-page-button-bar mt-1">
+          <div className="d-flex main-page-buttons mx-2 flex-column align-items-center">
+            <Link to={ROUTES.format} className="fullWidth">
+              <Button variant="primary" size="lg" className="fullWidth">Create Format</Button>
+            </Link>
+            <h6 className="text-muted">Requires Free Account</h6>
+          </div>
+          <div className="d-flex main-page-buttons mx-2 flex-column align-items-center">
+            <Link to={ROUTES.deck} className="fullWidth">
+              <Button variant="primary" size="lg" className="fullWidth">Build Deck</Button>
+            </Link>
+            <h6 className="text-muted">No Account Needed!</h6>
+          </div>
         </div>
       </div>
     );
