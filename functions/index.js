b82bf56e899e071f6d69b51dbb26bab525605aac
Jeff425 <goodhue.jeff@gmail.com>
2019-06-10 17:21:10 -0700

    Cloud functions now handles writing authorNames to new formats and timestamps

--- functions/index.js
+++ functions/index.js
@@ -42,3 +42,28 @@ exports.updateDisplayName = functions.https.onCall((data, context) => {
     });
   });
 });
+
+exports.writeFormatInfo = functions.firestore.document("formats/{formatId}").onCreate((snap, context) => {
+  const createStamp = admin.firestore.Timestamp.now();
+  let authName = "";
+  return admin.firestore().collection("users").doc(snap.data().author).get()
+  .then(authInfo => {
+    if (authInfo.exists) {
+      authName = authInfo.data().displayName;
+    }
+    return snap.ref.set({authorName: authName, createDate: createStamp, lastUpdate: createStamp}, {merge: true});
+  });
+});
+
+exports.updateFormatInfo = functions.firestore.document("formats/{formatId}").onUpdate((change, context) => {
+  const updateStamp = admin.firestore.Timestamp.now();
+  let createStamp = updateStamp;
+  if (change.before.data().createDate) {
+    createStamp = change.before.data().createDate;
+  }
+  let authName = change.before.data().authorName;
+  if (!authName) {
+    authName = "";
+  }
+  return change.after.ref.set({authorName: authName, createDate: createStamp, lastUpdate: updateStamp}, {merge: true});
+});
